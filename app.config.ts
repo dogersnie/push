f05f379cc3d662279c042aeba152259cd7cd9d95
Pushkin <pushk@mail>
2023-09-04 14:13:16 +0300

    test

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Kod.mobi',
+    description: 'Documentation Kod.Mobi',
+    aside: {
+      level: 0,
+      collapsed: true,
+    },
+    header: {
+      logo: false
+    },
+  }
+})
